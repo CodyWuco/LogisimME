7031ebbe63472a215c0caae4d03c6d5caa01e04b
SAK_NAM\tntma_000 <tntmak1@gmail.com>
2019-11-17 15:02:05 -0800

    did a fix for clickerinterface

--- app/src/main/java/com/gamecodeschool/logicsimulator/ClickInterface.java
+++ app/src/main/java/com/gamecodeschool/logicsimulator/ClickInterface.java
@@ -1,12 +1,14 @@
 package com.gamecodeschool.logicsimulator;
 
 public class ClickInterface {
-    LogicIcon AndSelection;
+    AbstractGridCell AndSelection;
     Grid grid;
 
     ClickInterface(){
         AbstractGridCell fakeGridNode = new EmptyGridCell(1, 1, 1, 1);
         AndSelection = new AndIcon(fakeGridNode);
+
+
 
     }
 
@@ -14,7 +16,8 @@ public class ClickInterface {
         grid = this.grid;
     }
 
-    setSelectionAnd(){
+    public void setSelectionAnd(){
         grid.selected = AndSelection;
+
     }
 }
