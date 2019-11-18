3869fbf8d155ded36fe3bccc1a481e173cee3c01
Joshua Olson <nguyent.joshua@gmail.com>
2019-11-17 16:00:43 -0800

    Added line class Line.java

---
+++ app/src/main/java/com/gamecodeschool/logicsimulator/Line.java
@@ -0,0 +1,8 @@
+package com.gamecodeschool.logicsimulator;
+
+public class Line {
+    private int x0,y0,x1,y1,strokeWidth;
+
+    public Line(int x0, int y0, int x1, int y1) { this.x0=x0; this.y0=y0; this.x1=x1; this.y1=y1;
+                                                  strokeWidth=1;}
+}
