17c318123aa9dae5db3eb072e186ee3cd255d403
Joshua T. Olson <joshuaolson@csus.edu>
2019-11-19 22:46:55 -0800

    added hypotenuse and completed midpoint for Wire class

--- app/src/main/java/com/gamecodeschool/logicsimulator/Wire.java
+++ app/src/main/java/com/gamecodeschool/logicsimulator/Wire.java
@@ -8,34 +8,54 @@ public class Wire {
     public Wire(int x0, int y0, int x1, int y1) { this.x0=x0; this.y0=y0; this.x1=x1; this.y1=y1;
                                                   strokeWidth=1;}
 
-    public float midPointInitialComponentX(float x) {
+    public int midPointInitialComponentX(float x) {
         float X;
         X = Math.round(x);
+        x0 = (int)X;
 
-        return X;
+        return (int)X;
     }
     public float midPointInitialComponentY(float y) {
         float Y;
         Y = Math.round(y);
+        y0 = (int)Y;
 
         return Y;
     }
     public float midPointFinalComponentX(float x) {
         float X;
         X = Math.round(x);
+        x1 = (int)x;
 
         return X;
     }
     public float midPointFinalComponentY(float y) {
         float Y;
         Y = Math.round(y);
+        y1 = (int)Y;
 
         return Y;
     }
-    public int midPointX(){}
-    public int midPointY(){}
+    public int midPointX(){
+        int x;
+        x = (x0+x1) / 2;
+        return x;
+    }
+    public int midPointY(){
+        int y;
+        y = (y0+y1) / 2;
+        return y;
+    }
+    //true midpoint
+    public int midPointXY(){
+        return 0; //not sure if needed
+    }
 
     public float moveEventX(float x) {return x;}
     public float moveEventY(float y) {return y;}
+
+    public int hypotenuse(){
+        //empty going back to later. This is to acquire right-angle lines
+    }
 
 }
