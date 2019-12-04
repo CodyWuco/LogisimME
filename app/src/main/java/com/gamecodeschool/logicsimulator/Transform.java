54ddef980a4c0d281620dc59b308ec496121159f
MehakdeepSingh07 <54616268+MehakdeepSingh07@users.noreply.github.com>
2019-12-04 07:07:27 -0800

    Added setter and getter methods in Transform.

--- app/src/main/java/com/gamecodeschool/logicsimulator/Transform.java
+++ app/src/main/java/com/gamecodeschool/logicsimulator/Transform.java
@@ -4,15 +4,32 @@ import android.graphics.PointF;
 
 class Transform {
     // for scrolling background
-    private int mXClip;
-    private boolean mReversedFirst = false;
+    private int xClip;
+    private boolean reversed = false;
 
-    private PointF location;
+    //private PointF location;
     private static PointF screenSize;
 
-    Transform(PointF startingLocation, PointF screenSize) {
-        location = startingLocation;
+    Transform(PointF screenSize) {
+        //location = startingLocation;
         this.screenSize = screenSize;
     }
+
+    int getXClip(){
+        return xClip;
+    }
+
+    void setXClip(int newXClip){
+        xClip = newXClip;
+    }
+
+    void flipReversedFirst(){
+        reversed = !reversed;
+    }
+
+    PointF getmScreenSize(){
+        return screenSize;
+    }
+
 }
 
