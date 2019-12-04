ca0d9aab5db38029668efd9ef56471029a249eb8
MehakdeepSingh07 <54616268+MehakdeepSingh07@users.noreply.github.com>
2019-12-04 04:55:33 -0800

    Created the instance variables and added a constructor for 'ObjectSpecific' class.

--- app/src/main/java/com/gamecodeschool/logicsimulator/ObjectSpecific.java
+++ app/src/main/java/com/gamecodeschool/logicsimulator/ObjectSpecific.java
@@ -1,4 +1,19 @@
 package com.gamecodeschool.logicsimulator;
 
+import android.graphics.PointF;
+
 public class ObjectSpecific {
+    private String tag;
+    private String bitmapName;
+    private float speed; //won't need it
+    private PointF sizeScale;
+    private String[] components;
+
+    ObjectSpecific(String tag, String bitmapName, PointF relativeScale,
+                   String[] components) {
+        this.tag = tag;
+        this.bitmapName = bitmapName;
+        this.sizeScale = relativeScale;
+        this.components = components;
+    }
 }
