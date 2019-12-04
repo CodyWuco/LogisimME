ef2c405425f21c2e9c56858d687c307b71574ed8
MehakdeepSingh07 <54616268+MehakdeepSingh07@users.noreply.github.com>
2019-12-04 06:57:04 -0800

    Added instance variables in Transform.java for scrolling background.

--- app/src/main/java/com/gamecodeschool/logicsimulator/Transform.java
+++ app/src/main/java/com/gamecodeschool/logicsimulator/Transform.java
@@ -3,6 +3,10 @@ package com.gamecodeschool.logicsimulator;
 import android.graphics.PointF;
 
 class Transform {
-// for scrolling background
+    // for scrolling background
+    private int mXClip;
+    private boolean mReversedFirst = false;
+
 
 }
+
